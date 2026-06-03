2d1a2300deb60f854e9fdadcf7fa3c72a51119f6
Andy Georges <ageorges@vpnz027.ugent.be>
2026-06-03 11:14:39 +0200

    fix: also minor version for the rebuild

--- build.sh
+++ build.sh
@@ -173,8 +173,8 @@ for rpm in $ORIGIN/rpmbuild/RPMS/x86_64/slurm-*$SUFFIX*.rpm ; do
 done 2>&1 | tee rpmrebuild-with-nvml.out
 
 # strip out torque binaries/wrapper from slurm-torque
-rpmrebuild -d $ORIGIN/dist --change-spec-files="sed '/\(pbsnodes\|mpiexec\|bin\/q.\+\)/d'" -p $ORIGIN/dist/x86_64/slurm-torque-*-${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist').nogpu.ug*.rpm
-rpmrebuild -d $ORIGIN/dist --change-spec-files="sed '/\(pbsnodes\|mpiexec\|bin\/q.\+\)/d'" -p $ORIGIN/dist/x86_64/slurm-torque-*-${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist').ug.*.rpm
+rpmrebuild -d $ORIGIN/dist --change-spec-files="sed '/\(pbsnodes\|mpiexec\|bin\/q.\+\)/d'" -p $ORIGIN/dist/x86_64/slurm-torque-*-${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist.%rhel_minor_version').nogpu.ug*.rpm
+rpmrebuild -d $ORIGIN/dist --change-spec-files="sed '/\(pbsnodes\|mpiexec\|bin\/q.\+\)/d'" -p $ORIGIN/dist/x86_64/slurm-torque-*-${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist.%rhel_minor_version').ug.*.rpm
 
 # get the RPMs out of the subdirectories
 find rpmbuild -type f -name "*.rpm" -exec rm {} ";"
