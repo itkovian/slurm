bfe6ba71b98c89a5e5dbce1fda3edf6a1809dc7f
Andy Georges <ageorges@vpne025.ugent.be>
2026-06-04 12:03:33 +0200

    fix: create proper release version

--- build.sh
+++ build.sh
@@ -162,7 +162,7 @@ rpmbuild -ba "${RPM_DEFINES[@]}" "${SLURM_BUILDOPTS[@]}" --without nvml \
 
 echo "Doing rpm rebuild (without nvml)"
 for rpm in $ORIGIN/rpmbuild/RPMS/x86_64/slurm-*$SUFFIX*.rpm ; do
-    rpmrebuild --release=${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist').nogpu.ug -d $ORIGIN/dist -p $rpm
+    rpmrebuild --release=${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist').${MINOR}.nogpu.ug -d $ORIGIN/dist -p $rpm
 done 2>&1 | tee rpmrebuild-without-nvml.out
 
 
@@ -173,7 +173,7 @@ rpmbuild -ba "${RPM_DEFINES[@]}" "${SLURM_BUILDOPTS[@]}" --with nvml \
 
 echo "Doing rpm rebuild (with nvml)"
 for rpm in $ORIGIN/rpmbuild/RPMS/x86_64/slurm-*$SUFFIX*.rpm ; do
-    rpmrebuild --release=${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist').ug -d $ORIGIN/dist -p $rpm
+    rpmrebuild --release=${OUR_RELEASE}.${GITTAG}$(rpm -E '%dist').${MINOR}.ug -d $ORIGIN/dist -p $rpm
 done 2>&1 | tee rpmrebuild-with-nvml.out
 
 MINOR=$(rpm -q --qf "%{VERSION}" redhat-release | cut -d. -f2)
